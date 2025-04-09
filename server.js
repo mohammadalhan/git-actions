4876811141767ade9e3856929cde3a72dd117aac
Mohammad Alhan <MohammadAlhanSamirkhan.Pathan@edu.sait.ca>
2025-04-09 16:57:18 -0600

    ALHAN

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:935871');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:ALHAN');
 });
 
 const server = app.listen(PORT, HOST);
